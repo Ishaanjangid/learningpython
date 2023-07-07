90cfc0539ebc42792c657fe8951855ac8f0df5c1
Ishaan <ishaanjangid035@gmail.com>
2023-07-07 13:03:08 +0530

    learning Precedence

---
+++ learning_basics.py
@@ -0,0 +1,23 @@
+### Learning/Revising basics of python With "Automate the Boring stuff with Python"
+
+
+print("Hello,World!")
+
+print(4/(3*2-1))
+
+print(4/3*(2-1))
+print(4+3%5)
+
+a =16
+b =15
+
+print(a%b//a)
+print(int(44.55))
+print(2**4.0)
+
+print(2**(3**2))
+print((2**3)**2)
+print(2**3**2)
+
+print(8/4/2)
+print(float(22//3+3/3))
