2ece34f70a0543bcd501e2ca356b3670bd7c6674
Ishaan <ishaanjangid035@gmail.com>
2023-07-14 11:17:28 +0530

    Day 8 Learning

--- Function_.py
+++ Function_.py
@@ -50,6 +50,89 @@ def f():
     name = "Ishaan"
     print("Ishaan")
 
-f()
 
-print(name)
+def spam():
+    global eggs
+
+    eggs = "spam"  #Global eggs
+
+
+def bacon():
+    eggs = "bacon"  #local eggs
+
+def ham():
+    print(eggs)  #This eggs refer to global eggs = "spam"
+
+eggs = 42 #This is global
+
+
+x = 12
+
+def f1(a,b=x):
+    
+    print(a,b)
+
+x = 15
+
+def f():
+    global a
+
+    print(a)
+
+    a = "hello"
+
+    print(a)
+
+a = "world"
+
+
+
+def f1(a,b = []):
+    b.append(a)
+    return b
+
+# print(f1(2,[3,4]))
+
+
+def f(p,q,r):
+    global s
+    p,q,r,s = 10,20,30,40
+    print(p,q,r,s)
+
+p,q,r,s = 1,2,3,4
+
+def f(x):
+    print("outer")
+    def f1(a):
+        print("inner")
+        print(a,x)
+
+f1(1)
+
+
+x = 5
+def f1():
+    global x
+    x = 4
+
+def f2(a,b):
+    global x
+    return a+b+x
+
+
+
+x = 100
+def f1():
+    global x
+    x = 90
+
+def f2():
+    global x
+    x = 80
+
+y,z = 1,2
+
+def f():
+    global x
+    x = y+z
+
