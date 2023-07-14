adbb3feae3760d619189cf48db297e70388baaf0
Ishaan <ishaanjangid035@gmail.com>
2023-07-14 09:18:05 +0530

    Day 9 Learning

---
+++ Function_.py
@@ -0,0 +1,55 @@
+### In this file I will practice function
+
+def hello(name):   # Defining the function
+    return(f"Hello,{name}")   # name => Parameter {local}
+
+# example  = hello("Ishaan")   # calling the function
+
+#                             # "Ishaan" passing the argumet   
+
+# print(type(example))
+
+
+def none():
+    return
+
+# print(print(none()))
+
+
+def f(t):
+    return t
+
+def maFonction(a):
+
+    i = 25 + a
+
+    i = f(i)
+
+    return i
+
+# c = maFonction(3)
+
+def a():
+    print("a() start")
+    b()
+    print("a() returns")
+
+def b():
+    print("b() start")
+    c()
+    print("b() returns")
+
+def c():
+    print("c() start")
+    print("c() returns")
+
+
+
+
+def f():
+    name = "Ishaan"
+    print("Ishaan")
+
+f()
+
+print(name)
