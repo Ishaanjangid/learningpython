e258024de1eea33d2a368ffea633027550225902
Ishaan <ishaanjangid035@gmail.com>
2023-08-07 10:40:57 +0530

    Day 25 Learning

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
-def ReturnsTrue():
-    print("ReturnsTrue() was called.")
-    return True
+# Step's 
 
-def ReturnsFalse():
-    print("ReturnsFalse() was called.")
-    return False
+# 1: Check that the board is full or not 
+## if: yes 'the board if full' or 'it's a tie' and exit the game
+## else: then  
 
-# print(ReturnsTrue() or ReturnsFalse())
-# print(ReturnsFalse() or ReturnsTrue())
+# 2: no 'then ask the player to make the move'
+# 2.1: then check is there space availabe for that move
 
-print(ReturnsTrue() and ReturnsFalse())
+## if: yes 'then make the move go to step 3'
+## else: no 'then go back to step 2'
+
+# 3: after that check the move is a WINNING move 
+## if: yes 'then congrulation' and 'exit the game'
+## else: no 'then continue and and switch turns between the player'
