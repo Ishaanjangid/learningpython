3af684243673b6cd250d011e5b91a046c7fabca3
Ishaan <ishaanjangid035@gmail.com>
2023-08-29 20:01:47 +0530

    Day 47 Learning

--- Day45&46&47.py
+++ Day45&46&47.py
@@ -1,11 +1,12 @@
+
+# Function that return a string of ALPHABET [a~z]
 def alphabet():
-    alphabet = []
+    alphabet = ''
 
     for i in range(65,91):
-        alphabet.append(chr(i))
+        alphabet += chr(i)
 
     return alphabet
-
 alph = alphabet()
 
 print("Enter Text...")
@@ -15,20 +16,93 @@ print("Enter keys:")
 keys = input('> ')
 keyA, keyB = keys.split()
 
-cryptic_txt = []
+# Function to ask user to (e)ncrypt or (d)ecrypt the message.
+def encryptOrDecryptMessage():
+    '''ask user to encrypt or decrypt a message'''
+    while True:
+        print('''Type (e) to 'encrypt' or,
+              (d) to 'decrypt'.''')
 
+        mode = input('> ')
 
-for i in text.split():
-    word = ''
-    for j in i:
+        if mode.lower().startswith('e'):
+            # User want to encrypt the message
+            myMode = 'encrypt'
         
-        letter_index = alph.index(j.upper())
-        keyA,keyB=int(keyA),int(keyB)
+        elif mode.lower().startswith('d'):
+            # User want to dcrypt the message
+            myMode = 'decrypt'
 
-        new_index = ((keyA * letter_index) + keyB) % 26
+        else:
+            print("INVALID INPUT!!!")
+            continue
 
-        word += alph[new_index]
+        return myMode
 
-    cryptic_txt.append(word)
+# Function to encrypt text
+def encryptText(message,keyA,keyB):
+    encryptic_txt = []
 
-print(' '.join(cryptic_txt))        
+    for i in message.split():
+        word = ''
+        for j in i:
+            
+            letter_index = alph.index(j.upper())
+            keyA,keyB=int(keyA),int(keyB)
+
+            new_index = ((keyA * letter_index) + keyB) % 26
+
+            word += alph[new_index]
+
+        encryptic_txt.append(word)
+
+    return (' '.join(encryptic_txt))
+
+# Function to decrypt text
+def decryptText(message,keyA,keyB):
+    '''To decrypt the, encrypt text'''
+    pass
+
+
+
+# Function to find the HCF
+def HCF(a,b):
+
+    factor_a = []
+    factor_b = []
+
+    # Calculating factor of 'a'
+
+    i = 2
+    while i <= a:
+
+        if a % i == 0:
+            factor_a.append(i)
+            a //= i
+        else:
+            i += 1
+
+
+    # Calculating factor of 'b'
+    
+    i = 2
+
+    while i <= b:
+        if b % i == 0:
+            factor_b.append(i)
+            b //= i
+        else:
+            i += 1
+
+    # Collecting the common factors
+
+    common_factor = set(factor_a) & set(factor_b)
+
+    hcf = 1
+
+    for factor in common_factor:
+        hcf *= factor
+
+    
+    return hcf
+
--- test.py
+++ test.py
@@ -2,49 +2,49 @@
 the github code by AL SWEIGERT'''
 
 
-# import random
+import random
 
 # SYMBOLS = """ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEF""" + \
 #           """GHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
 
-# def HCF(a,b):
-#     factor_a = []
-#     factor_b = []
+def HCF(a,b):
+    factor_a = []
+    factor_b = []
 
-#     # Calculating factor of 'a'
+    # Calculating factor of 'a'
 
-#     i = 2
-#     while i <= a:
+    i = 2
+    while i <= a:
 
-#         if a % i == 0:
-#             factor_a.append(i)
-#             a //= i
-#         else:
-#             i += 1
+        if a % i == 0:
+            factor_a.append(i)
+            a //= i
+        else:
+            i += 1
 
 
-#     # Calculating factor of 'b'
+    # Calculating factor of 'b'
     
-#     i = 2
+    i = 2
 
-#     while i <= b:
-#         if b % i == 0:
-#             factor_b.append(i)
-#             b //= i
-#         else:
-#             i += 1
+    while i <= b:
+        if b % i == 0:
+            factor_b.append(i)
+            b //= i
+        else:
+            i += 1
 
-#     # Collecting the common factors
+    # Collecting the common factors
 
-#     common_factor = set(factor_a) & set(factor_b)
+    common_factor = set(factor_a) & set(factor_b)
 
-#     hcf = 1
+    hcf = 1
 
-#     for factor in common_factor:
-#         hcf *= factor
+    for factor in common_factor:
+        hcf *= factor
 
     
-#     return hcf
+    return hcf
 
 # def genarateRandomKey():
 
@@ -76,15 +76,15 @@ the github code by AL SWEIGERT'''
 # # print(b)
 
 
-### Function to check if the key's are valid or not...
+# ### Function to check if the key's are valid or not...
 
-def checkKey(keyA, keyB,mode):
-    '''Check If the key's are valid or not'''
+# def checkKey(keyA, keyB,mode):
+#     '''Check If the key's are valid or not'''
 
-    if mode == 'encrypt' and keyA == 1 and keyB == 0:
-        print("This key effectively does not do any encryption on the")
-        print("message. Choose a different key.")
+#     if mode == 'encrypt' and keyA == 1 and keyB == 0:
+#         print("This key effectively does not do any encryption on the")
+#         print("message. Choose a different key.")
 
-        return False
+#         return False
     
-    elif keyA < 0 or keyB < 0 or 
+#     elif keyA < 0 or keyB < 0 or 
